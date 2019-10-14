8b0a3abb2b6b394abd068c28f3b4fb3830c02fac
Ghouse <mohammed.shaikh@niit.com>
2019-10-14 22:53:44 +0530

    Initial commit

--- src/main/java/com/stackroute/LuckyRegistrationNumber.java
+++ src/main/java/com/stackroute/LuckyRegistrationNumber.java
@@ -3,8 +3,7 @@ package com.stackroute;
 //Complete the code below as given in the problem.md file
 public class LuckyRegistrationNumber {
 
-    private static final String REGISTRATION_NUMBER_FORMAT = "(KA|DL)(0[1-9]|10)[A-Z]{1,2}([1-9]\\d{3})";
-
+   
     public static int checkRegistrationNumber(String regNumber) {
         return -2;
     }
